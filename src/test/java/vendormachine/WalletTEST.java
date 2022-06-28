7f9ee12e9d80b5643043ecbdf6edc4e927de524d
shakir-ali-Github <shakirali_@hotmail.co.uk>
2022-06-28 14:46:23 +0100

    updated WalletTest

--- src/test/java/vendormachine/WalletTEST.java
+++ src/test/java/vendormachine/WalletTEST.java
@@ -1,5 +1,9 @@
 package vendormachine;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 import vendormachine.users.util.Wallet;
 
 public class WalletTEST {
@@ -17,4 +21,21 @@ public class WalletTEST {
 	// Others Tests for other classes that use Wallet may impact code coverage
 	// Avoid relying on these tests, one test for one class; keep "Single Responsibility" in mind.
 	
+	@Test
+	public void testAddCredit() {
+		Wallet wallet1 = new Wallet();
+		wallet1.addCredit(1.5f);
+		assertEquals(2f, wallet1.getAllCredit());
+	}
+	
+	@Test
+	public void testGetCredit() {
+		Wallet wallet1 = new Wallet();
+		wallet1.getCredit(1.5f);
+		assertEquals(0.5f, wallet1.getCredit(0.5f));
+	}
+	
+
+	
 }
+
